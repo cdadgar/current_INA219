d63340cb234c364374fa2ccce4abc19879f41a65
Cyrus Dadgar <java_siphon@yahoo.com>
2020-06-12 08:10:46 -0400

    new code

---
+++ current_INA219.ino
@@ -0,0 +1,75 @@
+/*
+ * INA219 DC current sensor 
+ * 
+ * 
+ * part I have doesn't appear to work.
+ * when I hook up vdd and gnd, the power rails drops from 3.3v to 2v...not good
+ * ordered 2 more from china.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * use library:
+ * https://github.com/adafruit/Adafruit_INA219
+ *
+ * see https://learn.adafruit.com/adafruit-ina219-current-sensor-breakout 
+ * VDD - 3.3 or 5 VDC
+ * GND - ground
+ * SCL - i2c pins (on wemos: SDA => D2, SCL => D1)
+ * SDA - ic2 pins
+ * VIN-   current sensing pins
+ * VIN+
+ * also available on screw down terminals
+ */
+
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+Adafruit_INA219 ina219;
+
+
+void setup(void) 
+{
+  // start serial port
+  Serial.begin(115200);
+  Serial.print(F("\n\n INA219 test\n\n"));
+
+  // Initialize the INA219.
+  // By default the initialization will use the largest range (32V, 2A).  However
+  // you can call a setCalibration function to change this range (see comments).
+  if (! ina219.begin()) {
+    Serial.println("Failed to find INA219 chip");
+    while (1) { delay(10); }
+  }
+  // To use a slightly lower 32V, 1A range (higher precision on amps):
+  //ina219.setCalibration_32V_1A();
+  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+  //ina219.setCalibration_16V_400mA();
+
+  Serial.println("Measuring voltage and current with INA219 ...");
+}
+
+void loop(void) 
+{
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+  Serial.println("");
+
+  delay(2000);
+}
